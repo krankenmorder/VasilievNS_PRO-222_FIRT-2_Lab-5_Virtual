ff23eab5d96c02c2d026a3834da210599d48b6b6
Nikolai Vasiliev <63501861+krankenmorder@users.noreply.github.com>
2020-12-10 20:15:37 +0500

    version 1.3
    Динамическое создание объектов ранее созданных классов. Проверка на принадлежность к классам с помощью методов classname и isA.

--- LabOOP_5/LabOOP_5.cpp
+++ LabOOP_5/LabOOP_5.cpp
@@ -5,5 +5,22 @@ using namespace std;
 
 int main()
 {
-    
+    setlocale(LC_ALL, "Russian");
+
+    Tree* derevo_1 = new Tree();
+    Cedar* kedr_1 = new Cedar();
+    Tree* derevo_2 = new Cedar();
+
+    cout << "\n\nПроверка на принадлежность к классу с помощью метода classname:\n";
+    cout << derevo_1->classname() << endl;
+    cout << kedr_1->classname() << endl;
+    cout << derevo_2->classname() << endl;
+
+    cout << "\n\nПроверка на принадлежность к классу с помощью метода isA:\n";
+    cout << derevo_1->isA("Tree") << endl;
+    cout << kedr_1->isA("Cedar") << endl;
+    cout << derevo_2->isA("Tree") << endl;
+
+    system("pause");
+
 }
