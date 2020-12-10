ad5a346e8e61256d8f3fa96a0fa3a69483228600
Nikolai Vasiliev <63501861+krankenmorder@users.noreply.github.com>
2020-12-10 19:24:03 +0500

    version 1.1
    Создание в заголовочном файле двух классов Дерево и его наследника Кедр.

--- LabOOP_5/LabOOP_5.cpp
+++ LabOOP_5/LabOOP_5.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
+#include "myClasses.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
---
+++ LabOOP_5/myClasses.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Tree {
+private:
+	string ClassName = "Tree";
+public:
+	int height, width, age;
+	string type;
+
+	Tree() {
+		this->height = 0;
+		this->width = 0;
+		this->age = 0;
+		this->type = "";
+		cout << "Конструктор по умолчанию Tree.\n";
+	}
+
+	Tree(int a, int b, int c, string d) {
+		this->height = a;
+		this->width = b;
+		this->age = c;
+		this->type = d;
+		cout << "Конструктор с параметрами Tree.\n";
+	}
+
+	Tree(Tree &T) {
+		this->height = T.height;
+		this->width = T.width;
+		this->age = T.age;
+		this->type = T.type;
+		cout << "Конструктор копирования Tree.\n";
+	}
+
+	~Tree() {
+		cout << "Деструктор Tree.\n";
+	}
+};
+
+class Cedar : public Tree {
+private:
+	string ClassName = "Cedar";
+public:
+	Cedar() {
+		this->height = 44;
+		this->width = 2;
+		this->age = 800;
+		this->type = "Conifers";
+		cout << "Конструктор по умолчанию Cedar.\n";
+	}
+
+	Cedar(int a, int b, int c) {
+		this->height = a;
+		this->width = b;
+		this->age = c;
+		cout << "Конструктор с параметрами Cedar.\n";
+	}
+
+	Cedar(Cedar &C) {
+		this->height = C.height;
+		this->width = C.width;
+		this->age = C.age;
+		cout << "Конструктор копирования Cedar.\n";
+	}
+
+	~Cedar() {
+		cout << "Деструктор Cedar.\n";
+	}
+};
